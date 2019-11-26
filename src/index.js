ca45c21de8d430b9f789b8902db66fcb7cbae0c7
jcamilom <jcamilomunozz@gmail.com>
2019-11-25 19:26:17 -0500

    Creatig user form request body

--- src/index.js
+++ src/index.js
@@ -10,17 +10,9 @@ app.use(express.json())
 app.post('/login', (req, resp) => resp.send());
 app.post('/users', (req, resp) => {
   const birthday = req.body.birthday.split('/');
-  User.create({
-    name: 'Pedro',
-    surname: 'Cuartas',
-    birthday: new Date(+birthday[0], +birthday[1] - 1, +birthday[2]),
-    identification: '123456789',
-    phoneNumber: '332-543-3333',
-    profilePicture: 'http://1234',
-    email: 'pedro@mail.com',
-    password: 'pass1234'
-  }).then(user => {
-    resp.json(user);
+  req.body.birthday = new Date(+birthday[0], +birthday[1] - 1, +birthday[2])
+  User.create(req.body).then(user => {
+    resp.status(201).send(user);
   });
 });
 app.put('/users', () => console.log('update'));
