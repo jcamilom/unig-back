da549e34682e8d583cffc716dca5ddee726bdecf
jcamilom <jcamilomunozz@gmail.com>
2019-11-27 18:40:09 -0500

    Using user router

--- src/index.js
+++ src/index.js
@@ -1,93 +1,11 @@
 const express = require('express');
-
-const { User } = require('./db/db');
+const userRouter = require('./routers/user');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json())
-
-app.post('/login', async (req, resp) => {
-  try {
-    const user = await User.findOne({
-      where: {
-        email: req.body.email,
-        password: req.body.password
-      }
-    });
-
-    if (!user) {
-      return resp.status(404).send();
-    }
-    resp.send(user);
-  } catch (e) {
-    resp.status(500).send();
-  }
-});
-
-app.post('/users', async (req, resp) => {
-  try {
-    const user = await User.create(req.body);
-    resp.status(201).send(user);
-  } catch (e) {
-    if (e.name === 'SequelizeValidationError') {
-      return resp.status(400).send(e.message);
-    }
-    resp.status(500).send();
-  }
-});
-
-app.patch('/users/:id', async (req, resp) => {
-  try {
-    const user = await User.findByPk(req.params.id);
-
-    if (!user) {
-      return resp.status(404).send();
-    }
-    await User.update(req.body, {
-      where: {
-        id: req.params.id
-      }
-    });
-    resp.send();
-  } catch (e) {
-    if (e.name === 'SequelizeValidationError') {
-      return resp.status(400).send(e.message);
-    } else if (e.name === 'SequelizeUniqueConstraintError') {
-      return resp.status(400).send('Email is already being used');
-    }
-    console.log(e.name)
-    console.log(e.message)
-    resp.status(500).send();
-  }
-});
-
-app.delete('/users/:id', async (req, resp) => {
-  try {
-    const user = await User.findByPk(req.params.id);
-
-    if (!user) {
-      return resp.status(404).send();
-    }
-    await User.destroy({
-      where: {
-        id: user.id
-      }
-    })
-    resp.send();
-  } catch (e) {
-    resp.status(500).send();
-  }
-});
-
-app.get('/users', async (req, resp) => {
-  try {
-    const users = await User.findAll();
-    resp.send(users);
-  } catch (e) {
-    resp.status(500).send();
-  }
-});
+app.use(userRouter);
 
 app.listen(port, () => {
   console.log('app running');
